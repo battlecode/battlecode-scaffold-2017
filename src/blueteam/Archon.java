42ac707ce6b8df95d0c44a2a842e78eb27033b4f
Jiri Horner <laeqten@gmail.com>
2017-02-28 17:31:19 +0100

    archon: implement basic tasks
    * moving away from nearby enemies
    * buiding gardeners
    * buying victory points
    
    all very simplistic

--- src/blueteam/Archon.java
+++ src/blueteam/Archon.java
@@ -2,10 +2,12 @@ package blueteam;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
 
 public class Archon extends Robot {
+
+	private final int desiredNumberOfGardeners = 10;
 
 	Archon(RobotController rc) {
 		super(rc);
@@ -13,23 +15,27 @@ public class Archon extends Robot {
 
 	@Override
 	void step() throws GameActionException {
-		// Generate a random direction
-		Direction dir = randomDirection();
-
-		// Randomly attempt to build a gardener in this direction
-		if (rc.canHireGardener(dir) && Math.random() < .01) {
-			rc.hireGardener(dir);
+		// move away from enemy
+		RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
+		if (robots.length > 0) {
+			Direction toEnemy = rc.getLocation().directionTo(robots[0].getLocation());
+			tryMove(toEnemy.opposite());
+		} else {
+			tryMove(randomDirection());
 		}
 
-		// Move randomly
-		tryMove(randomDirection());
+		// randomly attempt to build a gardener if we need more
+		Direction dir = randomDirection();
+		int numOfGardeners = rc.readBroadcast(0);
+		if (numOfGardeners < desiredNumberOfGardeners && rc.canHireGardener(dir) && Math.random() < 0.5) {
+			rc.hireGardener(dir);
+			rc.broadcast(0, numOfGardeners + 1);
+		}
 
-		// Broadcast archon's location for other robots on the team to
-		// know
-		MapLocation myLocation = rc.getLocation();
-		rc.broadcast(0, (int) myLocation.x);
-		rc.broadcast(1, (int) myLocation.y);
-
+		// buy some victory points randomly
+		if (Math.random() < 0.05) {
+			rc.donate(rc.getTeamBullets() / 4);
+		}
 	}
 
 }
